04765fd68a9f483c25c5ec63d1a195bc0394e97d
William Lin <wlin26@yahoo.com>
2019-11-05 12:04:30 -0500

    Added public folder with site content

---
+++ src/public/precache-manifest.e19466a720d3a1e33d75a4879bc752a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43f9f6bb457aa0df33d861788e035793",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5757087a81eb300a714",
+    "url": "/static/css/main.1fab4212.chunk.css"
+  },
+  {
+    "revision": "f034de6881ca3b21166f",
+    "url": "/static/js/2.936ca1ab.chunk.js"
+  },
+  {
+    "revision": "e5757087a81eb300a714",
+    "url": "/static/js/main.6aa8c94e.chunk.js"
+  },
+  {
+    "revision": "2e4bc1b3c022e2e6153b",
+    "url": "/static/js/runtime-main.d54b910c.js"
+  }
+]);
