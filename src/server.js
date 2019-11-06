9fa6c1ba6e0483d083ea052abdc484117c8c7a6a
William Lin <wlin26@yahoo.com>
2019-11-06 15:22:51 -0500

    Added dotenv and mongoose to server

--- src/server.js
+++ src/server.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+require('dotenv').config();
 const app = express();
+
+const mongodbUri = process.env.MONGO_URI || 'mongodb://localhost:27017/test';
+const mongodbOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+};
+mongoose.connect(mongodbUri, mongodbOptions, (err) => {
+    if (err) {
+        console.error(`Unable to connecto MongoDB with error: ${err}`);
+    } else {
+        console.log('Connected to MongoDB.');
+    }
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 
