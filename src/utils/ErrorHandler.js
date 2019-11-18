f638a961760a2851e7fef0928679f7afc505aa0c
William Lin <wlin26@yahoo.com>
2019-11-18 14:27:59 -0500

    Added JS Docs to error handler

--- src/utils/ErrorHandler.js
+++ src/utils/ErrorHandler.js
@@ -1,6 +1,14 @@
 const chalk = require('chalk');
 
-function handleServerError(_, res, statusCode=500, error='', errorMessage='', logger=undefined) {
+/**
+ * @param {Request} _req Express HTTP request.
+ * @param {Response} res Express HTTP response.
+ * @param {number} statusCode Status code to send.
+ * @param {string} error Title or short description of error.
+ * @param {string} errorMessage Summary, message, or description of error.
+ * @param {*} logger Winston logger object to log to.
+ */
+function handleServerError(_req, res, statusCode=500, error='', errorMessage='', logger=undefined) {
     if (logger) logger.log({ level: 'error', error, errorMessage });
     if (statusCode < 500 || statusCode > 599) statusCode = 500;
     return res.status(statusCode).send({
@@ -9,7 +17,15 @@ function handleServerError(_, res, statusCode=500, error='', errorMessage='', lo
     });
 }
 
-function handleClientError(_, res, statusCode=400, error='', errorMessage='', logger=undefined) {
+/**
+ * @param {Request} _req Express HTTP request.
+ * @param {Response} res Express HTTP response.
+ * @param {number} statusCode Status code to send.
+ * @param {string} error Title or short description of error.
+ * @param {string} errorMessage Summary, message, or description of error.
+ * @param {*} logger Winston logger object to log to.
+ */
+function handleClientError(_req, res, statusCode=400, error='', errorMessage='', logger=undefined) {
     if (logger) logger.log({ level: 'error', error, errorMessage });
     if (statusCode < 400 || statusCode > 499) statusCode = 400;
     return res.status(statusCode).send({
@@ -18,6 +34,10 @@ function handleClientError(_, res, statusCode=400, error='', errorMessage='', lo
     });
 }
 
+/**
+ * Console logs with red chalk.
+ * @param {string} message Message to console log.
+ */
 function consoleError(message) {
     console.log(chalk.red(message));
 }
