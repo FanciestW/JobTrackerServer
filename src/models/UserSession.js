d812d3f05022d3931e96efc09f43d504c806ccb1
William Lin <wlin26@yahoo.com>
2019-11-18 16:13:28 -0500

    Fixed session expired timeout default schema

--- src/models/UserSession.js
+++ src/models/UserSession.js
@@ -5,7 +5,7 @@ const UserSessionSchema = Schema({
     sid: { type: String, unique: true, required: true },
     uid: { type: String, required: true },
     createdAt: { type: Date, default: Date.now, required: true },
-    expiresAt: { type: Date, expires: 3600, default: Date.now() + 3600 * 1000, required: true },
+    expiresAt: { type: Date, expires: 3600, default: () => Date.now() + 3600*1000, required: true },
 });
 
 module.exports = { UserSession: mongoose.model('User_Session', UserSessionSchema), UserSessionSchema };
